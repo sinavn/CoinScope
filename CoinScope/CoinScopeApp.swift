bd1cbfb522bf7d93dccf97a92a374c44644c5da9
SinaVn <vosogh858sina@gmail.com>
2024-08-12 14:32:15 +0330

    Initial Commit

---
+++ CoinScope/CoinScopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinScopeApp.swift
+//  CoinScope
+//
+//  Created by Sina Vosough Nia on 5/22/1403 AP.
+//
+
+import SwiftUI
+
+@main
+struct CoinScopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
