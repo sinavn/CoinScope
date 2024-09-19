95b607c408c29077d5c587d987d3e4fccc5fa4ff
SinaVn <vosogh858sina@gmail.com>
2024-09-19 20:00:57 +0330

    add missing file

--- CoinScope/Utilities/Constants.swift
+++ CoinScope/Utilities/Constants.swift
@@ -2,7 +2,14 @@
 //  Constants.swift
 //  CoinScope
 //
-//  Created by Sina Vosough Nia on 6/28/1403 AP.
+//  Created by Sina Vosough Nia on 5/23/1403 AP.
 //
 
 import Foundation
+
+struct Constants {
+    static let CGBaseURL = "https://api.coingecko.com/api/v3"
+    static let etherScanBseURL = "https://api.etherscan.io/api"
+    static let CGAPIkey = ProcessInfo.processInfo.environment["CG_API_key"] ?? ""
+    static let etherScanAPIKey =  ProcessInfo.processInfo.environment["ETHER_SCAN_APIKEY"] ?? ""
+}
